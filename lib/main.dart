946856f85f4941071389b86779f020106a7e38e8
Richard Roncancio <batusai513@msn.com>
2018-05-20 15:22:45 -0500

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,30 @@
-void main() {
-  print('Hola mundo');
+import 'package:flutter/material.dart';
+
+// main: application start point
+// runnApp: creates a new flutter App
+void main() => runApp(
+      HelloWorld(),
+    );
+
+class HelloWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Hell world',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Hello Worl'),
+          ),
+          body: Center(
+            child: Text('Hello World'),
+          ),
+        ));
+  }
 }
+
+// MaterialApp
+//  title:
+//  body:
+//    Scafold
+//      appBar
+//      body
